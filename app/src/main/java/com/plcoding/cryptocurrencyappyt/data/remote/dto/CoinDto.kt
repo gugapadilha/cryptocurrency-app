7eea73c206017ab4930083020d1325a0620ee294
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-10-12 14:01:45 -0300

    imp api and model

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/dto/CoinDto.kt
@@ -0,0 +1,23 @@
+package com.plcoding.cryptocurrencyappyt.data.remote.dto
+
+import com.plcoding.cryptocurrencyappyt.domain.model.Coin
+
+data class CoinDto(
+    val id: String,
+    val isActive: Boolean,
+    val isNew: Boolean,
+    val name: String,
+    val rank: Int,
+    val symbol: String,
+    val type: String
+)
+
+fun CoinDto.toCoin(): Coin {
+    return Coin(
+        id = id,
+        isActive = isActive,
+        name = name,
+        rank = rank,
+        symbol = symbol
+    )
+}
