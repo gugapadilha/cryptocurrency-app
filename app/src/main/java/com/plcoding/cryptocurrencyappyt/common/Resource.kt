4da292d6e4537cd4a7cb9f10a912c78293b3ec6f
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-10-12 17:18:36 -0300

    impl the use cases

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/common/Resource.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/common/Resource.kt
@@ -2,7 +2,7 @@ package com.plcoding.cryptocurrencyappyt.common
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
 
-    class Sucess<T>(data: T) : Resource<T>(data)
+    class Success<T>(data: T) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
     class Loading<T>(data: T? = null) : Resource<T>(data)
 
